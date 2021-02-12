c6f212c38a9b00d14ce51bbfbdd00eda8d92ee43
luizh.nnh@gmail.com <Luiz Nascimento>
2021-02-12 02:00:24 -0300

    feat: adicionado server service responsável por inciar o micro serviço

---
+++ index.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+import ServerService from "./src/services/ServerService";
+dotenv.config();
+
+const server: ServerService = new ServerService();
+const portNumber: string = process.env.APPLICATION_PORT || process.env.PORT || "3333"
+const port: number = parseInt(portNumber, 10);
+server.start(port);
